3bad0b5705dd2b35b4f6f3610763e19145bfe3e3
yeasirahnaf <yeasir.ahnaf.1997@gmail.com>
2024-12-30 18:34:00 +0600

    Add power function

--- project.py
+++ project.py
@@ -13,9 +13,13 @@ def divide(a, b):
 def subtract(a, b):
     return a - b
 
+def power(a, b):
+    return a**b
+
 if __name__ == "__main__":
     print(greet("World"))
     print(add(2, 3))
     print(multiply(4, 5))
     print(divide(10, 2))
     print(subtract(10, 5))
+    print(power(2,3))
