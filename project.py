8877b8012f3a01548194a9714508e63026b55232
yeasirahnaf <yeasir.ahnaf.1997@gmail.com>
2024-12-30 18:42:24 +0600

    Improve test cases

--- project.py
+++ project.py
@@ -42,9 +42,9 @@ if __name__ == "__main__":
     print()
 
 if __name__ == "__main__":
-    print(add(2,3))
-    print(multiply(4,5))
-    print(divide(10,2))
-    print(subtract(10,5))
-    print(power(2,3))
-    print(modulo(7,3))
+    print(add(2,3)) #5
+    print(multiply(4,5)) #20
+    print(divide(10,2)) #5.0
+    print(subtract(10,5)) #5
+    print(power(2,3)) #8
+    print(modulo(7,3)) #1
