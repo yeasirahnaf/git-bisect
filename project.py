bcc95e854a292ca1567e3260cdf8c25bd6d4fed9
yeasirahnaf <yeasir.ahnaf.1997@gmail.com>
2024-12-30 18:32:23 +0600

    Add subtraction function

--- project.py
+++ project.py
@@ -10,8 +10,12 @@ def multiply(a, b):
 def divide(a, b):
     return a / b
 
+def subtract(a, b):
+    return a - b
+
 if __name__ == "__main__":
     print(greet("World"))
     print(add(2, 3))
     print(multiply(4, 5))
     print(divide(10, 2))
+    print(subtract(10, 5))
